9d028db47b3045e158e399cfd846c2ccea82e407
Maximo Frank Cacio Garcia <168673125+PanesitoDead@users.noreply.github.com>
2025-06-09 12:13:11 -0500

    Update economia.php4

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Perú regresa al mercado global con bonos en soles y oferta de recompra de deuda",
+"autor" => "El peruano",
+"resumen" => "Perú está ofreciendo recomprar bonos con vencimiento en 2026, 2028, 2029 y 2031",
 ];
 ?>
