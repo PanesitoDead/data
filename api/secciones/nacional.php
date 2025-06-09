0d7f7b92afe672a23ab94a501f4771e569f49c99
Maximo Frank Cacio Garcia <168673125+PanesitoDead@users.noreply.github.com>
2025-06-09 12:11:52 -0500

    Update nacional.php3

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Aprobaron otro viaje a Dina Boluarte",
+"autor" => "El Diario Peruano",
+"resumen" => "Contra todo pronostico, la corrupcion se visualiza en todo su esplendor",
 ];
 ?>
