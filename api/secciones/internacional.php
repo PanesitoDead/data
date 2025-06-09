6b5ea3ef58c7438dcd98c95c09a2936e6b2f8bcd
Maximo Frank Cacio Garcia <168673125+PanesitoDead@users.noreply.github.com>
2025-06-09 12:10:54 -0500

    Update internacional.php1

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Portugal gano contra españa",
+"autor" => "El Comercio",
+"resumen" => "CR7 mejor que Lamine Yamal",
 ];
 ?>
